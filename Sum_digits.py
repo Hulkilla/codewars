0bc0908db2e5c7edea32f4d4f844c975b60c1330
Hulkilla <marinafd1993@gmail.com>
2024-02-03 17:16:27 +0100

    mejorando el código

--- Sum_digits.py
+++ Sum_digits.py
@@ -1,10 +1,8 @@
 digit = -4567
 
+digit = abs(digit)
 suma = 0
 for i in range(len(str(digit))):
-    if str(digit)[i] == "-":
-        pass
-    else:
-       suma += (int(str(digit)[i]))
+    suma += (int(str(digit)[i]))
 
 print(suma)
